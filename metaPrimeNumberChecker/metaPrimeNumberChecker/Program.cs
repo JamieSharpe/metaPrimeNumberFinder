d48f41b49587aae35b43009e32faf892d67edb2b
Jamie Sharpe <github@jamiesharpe.co.uk>
2013-10-23 03:13:27 +0100

    v1 commit
    Created meta program that creates the program source code that checks if
    the input is a prime number.

---
+++ metaPrimeNumberChecker/metaPrimeNumberChecker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace metaPrimeNumberChecker
+{
+    class MainClass
+    {
+        public static void Main ( string[] args )
+        {
+            String programSourceHeader = "using System;\n\nclass PrimeNumberChecker\n{\n\tstatic void Main()\n\t{\n\t\tConsole.WriteLine(\"Input a number\");\n\n\t\tint input = Convert.ToInt32(Console.ReadLine());\n\t\tint primeCount = 0;\n\t\tif (";
+
+            String programSourceBody = "";
+
+            for ( int i = 2; i < int.MaxValue; ++i )
+            {
+                int divisibleNumbers = 0;
+
+                for ( int j = 1; ( j <= i ) && ( divisibleNumbers < 3 ); ++j )
+                {
+                    if ( i % j == 0 )
+                    {
+                        divisibleNumbers++;
+                    }
+                }
+
+                if ( divisibleNumbers == 2 )
+                {
+                    programSourceBody += "\n\t\t\t(" + i + " == input) ||";
+                }
+            }
+
+            programSourceBody += " false";
+
+
+            String programSourceFooter = ")\n\t\t{\n\t\t\tConsole.WriteLine( input + \" is prime.\");\n\t\t\tprimeCount++;\n\t\t\tConsole.WriteLine(primeCount + \" total prime numbers found\");\n\t\t}\n\t}\n}";
+
+            String programSourceComplete = programSourceHeader + programSourceBody + programSourceFooter;
+
+            StreamWriter sw = new StreamWriter( "PrimeNumberChecker.cs", false );
+
+            sw.WriteLine( programSourceComplete );
+
+            sw.Close();
+        }
+    }
+}
