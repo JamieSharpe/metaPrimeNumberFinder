cc21b9b81a2d8d45db0d87abe7d3610a11ae99f5
Jamie Sharpe <github@jamiesharpe.co.uk>
2015-04-08 19:30:40 +0100

    string formatting

--- metaPrimeNumberChecker/metaPrimeNumberChecker/Program.cs
+++ metaPrimeNumberChecker/metaPrimeNumberChecker/Program.cs
@@ -7,11 +7,16 @@ namespace metaPrimeNumberChecker
     {
         public static void Main ( string[] args )
         {
-            String programSourceHeader = "using System;\n\nclass PrimeNumberChecker\n{\n\tstatic void Main()\n\t{\n\t\tConsole.WriteLine(\"Input a number\");\n\n\t\tint input = Convert.ToInt32(Console.ReadLine());\n\t\tint primeCount = 0;\n\t\tif (";
+            String programSourceHeader =    "using System;\n\n"+
+                                            "class PrimeNumberChecker\n"+
+                                            "{\n"+
+                                            "\tstatic void Main()\n"+
+                                            "\t{\n\t\tConsole.WriteLine(\"Input a number\");\n\n\t\tint input = Convert.ToInt32(Console.ReadLine());\n\t\tint primeCount = 0;\n\t\tif (";
 
             String programSourceBody = "";
 
-            for ( int i = 2; i < int.MaxValue; ++i )
+            //for ( int i = 2; i < int.MaxValue; ++i )
+            for ( int i = int.MaxValue - 1000; i < int.MaxValue; ++i )
             {
                 int divisibleNumbers = 0;
 
@@ -27,10 +32,10 @@ namespace metaPrimeNumberChecker
                 {
                     programSourceBody += "\n\t\t\t(" + i + " == input) ||";
                 }
+                Console.WriteLine( "Processing: " + i );
             }
 
             programSourceBody += " false";
-
 
             String programSourceFooter = ")\n\t\t{\n\t\t\tConsole.WriteLine( input + \" is prime.\");\n\t\t\tprimeCount++;\n\t\t\tConsole.WriteLine(primeCount + \" total prime numbers found\");\n\t\t}\n\t}\n}";
 
